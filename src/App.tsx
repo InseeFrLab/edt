519ff763b2212f8118beab3acb760fda61f4f40e
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-09-23 17:06:28 +0200

    feat(init): add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Online, Offline } from "react-detect-offline";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Online>Vous êtes en mode en ligne !</Online>
+            <Offline>Vous êtes en mode hors ligne !</Offline>
         </div>
     );
 }
