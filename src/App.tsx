ff8144013f0719d1e1798439876cfe7d8e64178f
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-09-28 10:11:22 +0200

    feat(init): add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,30 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { OrchestratorForStories } from "./orchestrator/Orchestrator";
 import source from "./local-test-source.json";
 import { theme } from "lunatic-edt/dist/ui/theme/theme";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import frFile from "./lang/fr.json";
+
+i18n.use(initReactI18next).init({
+    resources: {
+        fr: {
+            translation: frFile,
+        },
+    },
+    lng: "fr",
+    fallbackLng: "fr",
+    interpolation: {
+        escapeValue: false,
+    },
+});
+
 function App() {
+    const { t } = useTranslation();
+
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline enableColorScheme />
+            <header>{t("common.welcome")}</header>
             <Home></Home>
             {/*<OrchestratorForStories source={source} data={{}}></OrchestratorForStories>*/}
         </ThemeProvider>
