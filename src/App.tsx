2bbba594317bb1c86720ac076c3fa0d2a6ae7cf9
jean <jean.haderer@keyconsulting.fr>
2022-09-29 15:55:27 +0200

    feat(init): fetch questionnaire from pogues

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { Component, useEffect } from "react";
 import "./App.scss";
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { OrchestratorForStories } from "./orchestrator/Orchestrator";
-import source from "./local-test-source.json";
 import { theme } from "lunatic-edt";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
@@ -23,14 +22,40 @@ i18n.use(initReactI18next).init({
     },
 });
 
-function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline enableColorScheme />
-            <Home></Home>
-            <OrchestratorForStories source={source} data={{}}></OrchestratorForStories>
-        </ThemeProvider>
-    );
+type Source = {
+    source?: object
 }
 
-export default App;
+export default class App extends Component<{}, Source>{
+    
+    constructor(props: object) {
+        super(props);
+    
+        this.state = {
+          source: undefined
+        };
+      }
+
+    componentWillMount() {
+        const param = new URLSearchParams(window.location.search);
+        let url = 'https://pogues-back-office-insee.k8s.keyconsulting.fr/api/persistence/questionnaire/json-lunatic/l8lq5lp6'
+        if (param.has('questionnaire')) {
+            url = param.get('questionnaire')!;
+        }
+        fetch(url)
+            .then((sourcePromise) => sourcePromise.json())
+            .then((source) => this.setState({source: source}));;
+    }
+
+    render() {
+        return this.state.source ? 
+                (
+                    <ThemeProvider theme={theme}>
+                        <CssBaseline enableColorScheme />
+                        <Home></Home>
+                        <OrchestratorForStories source={this.state.source!} data={{}}></OrchestratorForStories>
+                    </ThemeProvider>
+                ) :
+                (<div>Chargement du questionnaire...</div>);        
+    }
+}
