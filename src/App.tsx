12b4936b80137605a9dea969e492e8b7b342eed1
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-09-30 18:05:43 +0200

    fix(init): refact

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { OrchestratorForStories } from "./orchestrator/Orchestrator";
 import { theme } from "lunatic-edt";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import frFile from "./lang/fr.json";
+import frFile from "./i18n/fr.json";
 import Home from "./page/home/Home";
 
 i18n.use(initReactI18next).init({
@@ -23,39 +23,39 @@ i18n.use(initReactI18next).init({
 });
 
 type Source = {
-    source?: object
-}
+    source?: object;
+};
 
-export default class App extends Component<{}, Source>{
-    
+export default class App extends Component<{}, Source> {
     constructor(props: object) {
         super(props);
-    
+
         this.state = {
-          source: undefined
+            source: undefined,
         };
-      }
+    }
 
     componentWillMount() {
         const param = new URLSearchParams(window.location.search);
-        let url = 'https://pogues-back-office-insee.k8s.keyconsulting.fr/api/persistence/questionnaire/json-lunatic/l8lq5lp6'
-        if (param.has('questionnaire')) {
-            url = param.get('questionnaire')!;
+        let url =
+            "https://pogues-back-office-insee.k8s.keyconsulting.fr/api/persistence/questionnaire/json-lunatic/l8lq5lp6";
+        if (param.has("questionnaire")) {
+            url = param.get("questionnaire")!;
         }
         fetch(url)
-            .then((sourcePromise) => sourcePromise.json())
-            .then((source) => this.setState({source: source}));;
+            .then(sourcePromise => sourcePromise.json())
+            .then(source => this.setState({ source: source }));
     }
 
     render() {
-        return this.state.source ? 
-                (
-                    <ThemeProvider theme={theme}>
-                        <CssBaseline enableColorScheme />
-                        <Home></Home>
-                        <OrchestratorForStories source={this.state.source!} data={{}}></OrchestratorForStories>
-                    </ThemeProvider>
-                ) :
-                (<div>Chargement du questionnaire...</div>);        
+        return this.state.source ? (
+            <ThemeProvider theme={theme}>
+                <CssBaseline enableColorScheme />
+                <Home></Home>
+                <OrchestratorForStories source={this.state.source!} data={{}}></OrchestratorForStories>
+            </ThemeProvider>
+        ) : (
+            <div>Chargement du questionnaire...</div>
+        );
     }
 }
