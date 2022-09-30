85f6241b67cf62e5542f634ee92c2aaa67cd6e76
NicoLaval <nicolas.laval@insee.fr>
2022-10-01 00:53:29 +0200

    fix: remove useless react-app-rewired
    if you need a custom script, please create it and preserve basic script as it

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const { resolve } = require("path");
-module.exports = function override(config, env) {
-    config.resolve.alias["react"] = path.resolve('./node_modules/react');
-    config.resolve.alias["react-dom"] = path.resolve('./node_modules/react-dom');
-    console.log(config);
-    return config;
-}
