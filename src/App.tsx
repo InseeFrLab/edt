d36c0530fef3d0f1bee3b50f22bdd109191df50d
jean <jean.haderer@keyconsulting.fr>
2022-10-03 18:22:12 +0200

    feat(init) : extends Component??? Seriously Functionnal components are state of art since weeks, years, no?
    Even if you decide to make vintage React code, you really want to call a Will method?
    
    et? why?
    Please, use environment variables to enable clean configuration instead of giving hard coding URLs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "./App.scss";
 
 import { CssBaseline } from "@mui/material";
@@ -22,40 +22,28 @@ i18n.use(initReactI18next).init({
     },
 });
 
-type Source = {
-    source?: object;
-};
+const App = () => {
+    const data = {};
+    const [source, setSource] = useState(null as object | null);
 
-export default class App extends Component<{}, Source> {
-    constructor(props: object) {
-        super(props);
+    useEffect(() => {
+        // this is temporary !!! TODO : replace when we know how we shoulmd do it ! This was to prenvent a source.json in the repo
+        const url = "https://pogues-back-office-insee.k8s.keyconsulting.fr/api/persistence/questionnaire/json-lunatic/l8lq5lp6";
 
-        this.state = {
-            source: undefined,
-        };
-    }
-
-    componentWillMount() {
-        const param = new URLSearchParams(window.location.search);
-        let url =
-            "https://pogues-back-office-insee.k8s.keyconsulting.fr/api/persistence/questionnaire/json-lunatic/l8lq5lp6";
-        if (param.has("questionnaire")) {
-            url = param?.get("questionnaire") ?? "";
-        }
         fetch(url)
             .then(sourcePromise => sourcePromise.json())
-            .then(source => this.setState({ source: source }));
-    }
-    render() {
-        const { source = {} } = this.state;
-        return this.state.source ? (
-            <ThemeProvider>
-                <CssBaseline enableColorScheme />
-                <Home></Home>
-                <OrchestratorForStories source={source} data={{}}></OrchestratorForStories>
-            </ThemeProvider>
-        ) : (
-            <div>Chargement du questionnaire...</div>
-        );
-    }
+            .then(source => setSource(source));
+    }, []);
+
+    return source && data ? (
+        <ThemeProvider>
+            <CssBaseline enableColorScheme />
+            <Home></Home>
+            <OrchestratorForStories source={source} data={data}></OrchestratorForStories>
+        </ThemeProvider>
+    ) : (
+        <div>Chargement du questionnaire...</div>
+    );
 }
+
+export default App;
