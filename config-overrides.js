afb5a580e449fe3eb9da517942d83d12f629a1a9
jean <jean.haderer@keyconsulting.fr>
2022-10-04 18:41:24 +0200

    remove optimization

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,20 @@
 const path = require("path");
 
+
+
 module.exports = function override(config, env) {
     config.resolve.alias["react"] = path.resolve('./node_modules/react');
     config.resolve.alias["react-dom"] = path.resolve('./node_modules/react-dom');
+
+    config.optimization = {
+        minimize: true,
+        splitChunks: {
+            chunks: 'all',
+            name: true
+        },
+        runtimeChunk: true
+    };
+
     console.log(config);
     return config;
 }
