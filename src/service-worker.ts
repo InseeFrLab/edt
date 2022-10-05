2f8bbafd7b4922f9758dfd4af07f4a243d642bd0
jean <jean.haderer@keyconsulting.fr>
2022-10-05 10:22:56 +0200

    feat(offline): Make it network first and cache all urls (image and lunatic json included)

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -9,10 +9,9 @@
 // service worker, and the Workbox build step will be skipped.
 
 import { clientsClaim } from "workbox-core";
-import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
+import { NetworkFirst } from "workbox-strategies";
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -53,19 +52,13 @@ registerRoute(
     createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html"),
 );
 
-// An example runtime caching route for requests that aren't handled by the
-// precache, in this case same-origin .png requests like those from in public/
+// cache every url for offline mode
 registerRoute(
-    // Add in any other file extensions or routing criteria as needed.
-    ({ url }) => url.origin === self.location.origin && url.pathname.endsWith(".png"),
+    // Cache all url.
+    ({ }) => true,
     // Customize this strategy as needed, e.g., by changing to CacheFirst.
-    new StaleWhileRevalidate({
-        cacheName: "images",
-        plugins: [
-            // Ensure that once this runtime cache reaches a maximum size the
-            // least-recently used images are removed.
-            new ExpirationPlugin({ maxEntries: 50 }),
-        ],
+    new NetworkFirst({
+        cacheName: "data"
     }),
 );
 
