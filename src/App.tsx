ed99967c31c7bc0bc777bb51efb37dcdaa6cbf4b
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-10-20 17:43:20 +0200

    fix(responsive-routing): better separation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import "i18n/i18n";
 import { theme } from "lunatic-edt";
-import { BrowserRouter } from "react-router-dom";
 import EdtRoutes from "routes/EdtRoutes";
 
 const App = () => {
@@ -9,9 +8,7 @@ const App = () => {
         <>
             <ThemeProvider theme={theme}>
                 <CssBaseline enableColorScheme />
-                <BrowserRouter>
-                    <EdtRoutes />
-                </BrowserRouter>
+                <EdtRoutes />
             </ThemeProvider>
         </>
     );
--- src/routes/EdtRoutes.tsx
+++ src/routes/EdtRoutes.tsx
@@ -4,19 +4,21 @@ import HomePage from "pages/home/Home";
 import NotFoundPage from "pages/not-found/NotFound";
 import OrchestratorPage from "pages/orchestrator/Orchestrator";
 import WhoAreYouPage from "pages/who-are-you/WhoAreYou";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const EdtRoutes = () => {
     return (
-        <Routes>
-            <Route path="*" element={<NotFoundPage />} />
-            <Route path="/" element={<HomePage />} />
-            <Route path="help" element={<HelpPage />} />
-            <Route path="activity" element={<ActivityPage />} />
-            <Route path="who-are-you" element={<WhoAreYouPage />} />
-            {/* DEV : dev purpose only*/}
-            <Route path="orchestrator" element={<OrchestratorPage />} />
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element={<NotFoundPage />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="help" element={<HelpPage />} />
+                <Route path="activity" element={<ActivityPage />} />
+                <Route path="who-are-you" element={<WhoAreYouPage />} />
+                {/* DEV : dev purpose only*/}
+                <Route path="orchestrator" element={<OrchestratorPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
