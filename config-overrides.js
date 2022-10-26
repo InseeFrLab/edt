1bf38dc2110e506e13fbb91b1d315f9dfd22129c
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-10-26 12:55:34 +0200

    fix(hourchecker): custom theme issue

--- config-overrides.js
+++ config-overrides.js
@@ -3,5 +3,8 @@ const path = require("path");
 module.exports = function override(config, env) {
     config.resolve.alias["react"] = path.resolve('./node_modules/react');
     config.resolve.alias["react-dom"] = path.resolve('./node_modules/react-dom');
+    config.resolve.alias["@emotion/react"] = path.resolve('./node_modules/@emotion/react');
+    config.resolve.alias["@mui/material"] = path.resolve('./node_modules/@mui/material');
+    config.resolve.alias["@inseefr/lunatic"] = path.resolve('./node_modules/@inseefr/lunatic');
     return config;
 }
