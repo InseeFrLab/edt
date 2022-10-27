9448bce9322c6ed718770b38fcb5d355e39d0006
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-10-27 12:10:58 +0200

    feat(addActivityOrRoute): dev purpose

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,7 @@ import { Button } from "@mui/material";
 import reminder_note from "assets/illustration/reminder-note.svg";
 import FlexCenter from "components/commons/FlexCenter/FlexCenter";
 import FlexEnd from "components/commons/FlexEnd/FlexEnd";
+import AddActivityOrRoute from "components/edt/AddActivityOrRoute/AddActivityOrRoute";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
@@ -32,6 +33,10 @@ const HomePage = () => {
                     <img src={reminder_note} alt={t("asset.reminder-notes-alt")} />
                 </FlexCenter>
                 <header>{t("home-page.welcome")}</header>
+                <AddActivityOrRoute
+                    labelledBy={t("accessibility.component.add-activity-or-route.labelled-by")}
+                    describedBy={t("accessibility.component.add-activity-or-route.described-by")}
+                />
             </div>
         </>
     );
