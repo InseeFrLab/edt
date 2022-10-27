a901e6a91c128a142d1776d69d356beae53b789a
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-10-27 12:09:01 +0200

    fix(responsive-routing): better routing, add tss-react

--- src/routes/EdtRoutes.tsx
+++ src/routes/EdtRoutes.tsx
@@ -6,20 +6,32 @@ import OrchestratorPage from "pages/orchestrator/Orchestrator";
 import WhoAreYouPage from "pages/who-are-you/WhoAreYou";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+const enum EdtRoutesNameEnum {
+    HELP = "help",
+    ACTIVITY = "activity",
+    WHO_ARE_YOU = "who-are-you",
+    //dev purpose only
+    ORCHESTRATOR = "orchestrator",
+}
+
 const EdtRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="*" element={<NotFoundPage />} />
                 <Route path="/" element={<HomePage />} />
-                <Route path="help" element={<HelpPage />} />
-                <Route path="activity" element={<ActivityPage />} />
-                <Route path="who-are-you" element={<WhoAreYouPage />} />
+                <Route path={EdtRoutesNameEnum.HELP} element={<HelpPage />} />
+                <Route path={EdtRoutesNameEnum.ACTIVITY} element={<ActivityPage />} />
+                <Route path={EdtRoutesNameEnum.WHO_ARE_YOU} element={<WhoAreYouPage />} />
                 {/* DEV : dev purpose only*/}
-                <Route path="orchestrator" element={<OrchestratorPage />} />
+                <Route path={EdtRoutesNameEnum.ORCHESTRATOR} element={<OrchestratorPage />} />
             </Routes>
         </BrowserRouter>
     );
 };
 
-export default EdtRoutes;
+const getNavigatePath = (page: EdtRoutesNameEnum): string => {
+    return "/" + page;
+};
+
+export { EdtRoutes, EdtRoutesNameEnum, getNavigatePath };
