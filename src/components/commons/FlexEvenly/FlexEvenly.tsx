342df51cce6880db27fa9cd21e812777e088ff23
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-10-28 12:19:11 +0200

    feat(error): better common components

--- src/components/commons/FlexEvenly/FlexEvenly.tsx
+++ src/components/commons/FlexEvenly/FlexEvenly.tsx
@@ -1,17 +1,17 @@
 import { Box } from "@mui/material";
 
-interface FlexEndProps {
+interface FlexEvenlyProps {
     children: JSX.Element[] | JSX.Element;
     className?: string;
 }
 
-const FlexEnd = (props: FlexEndProps) => {
+const FlexEvenly = (props: FlexEvenlyProps) => {
     const { children, className } = props;
     return (
-        <Box sx={{ display: "flex", justifyContent: "flex-end" }} className={className}>
+        <Box sx={{ display: "flex", justifyContent: "space-evenly" }} className={className}>
             {children}
         </Box>
     );
 };
 
-export default FlexEnd;
+export default FlexEvenly;
