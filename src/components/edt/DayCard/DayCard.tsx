8c79813d5e0198b5df726435b81f003b24879d58
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-11-10 09:28:04 +0100

    feat(components): new components

---
+++ src/components/edt/DayCard/DayCard.tsx
@@ -0,0 +1,67 @@
+import Box from "@mui/material/Box";
+import PersonSunIcon from "assets/illustration/card/person-sun.svg";
+import FlexCenter from "components/commons/FlexCenter/FlexCenter";
+import PourcentProgress from "components/edt/PourcentProgress/PourcentProgress";
+import { useTranslation } from "react-i18next";
+import { makeStyles } from "tss-react/mui";
+
+interface DayCardProps {
+    labelledBy: string;
+    describedBy: string;
+    onClick(): void;
+}
+
+const DayCard = (props: DayCardProps) => {
+    const { labelledBy, describedBy, onClick } = props;
+    const { classes } = useStyles();
+    const { t } = useTranslation();
+
+    return (
+        <FlexCenter>
+            <Box
+                aria-labelledby={labelledBy}
+                aria-describedby={describedBy}
+                className={classes.dayCardBox}
+                onClick={onClick}
+            >
+                <Box className={classes.leftBox}>
+                    <Box className={classes.iconBox}>
+                        <img src={PersonSunIcon} alt={t("accessibility.asset.card.person-sun-alt")} />
+                    </Box>
+                    <Box>
+                        <Box>{t("component.day-card.day")}</Box>
+                        <Box>{t("common.user.person")}</Box>
+                    </Box>
+                </Box>
+                <Box>
+                    <PourcentProgress labelledBy={""} describedBy={""} progress={"0"} />
+                </Box>
+            </Box>
+        </FlexCenter>
+    );
+};
+
+const useStyles = makeStyles({ "name": { DayCard } })(theme => ({
+    dayCardBox: {
+        width: "90%",
+        border: "1px solid transparent",
+        borderRadius: "10px",
+        padding: "1rem",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: theme.variables.white,
+        color: theme.palette.primary.light,
+        marginTop: "1rem",
+        cursor: "pointer",
+    },
+    leftBox: {
+        display: "flex",
+        alignItems: "center",
+    },
+    iconBox: {
+        marginRight: "1rem",
+    },
+}));
+
+export default DayCard;
