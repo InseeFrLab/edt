6a8a74515503e32e9926649d82862eaf1752f394
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-11-10 16:51:07 +0100

    feat(home): init home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
-import CodeIcon from "@mui/icons-material/Code";
 import HelpIcon from "@mui/icons-material/Help";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import reminder_note from "assets/illustration/reminder-note.svg";
 import FlexCenter from "components/commons/FlexCenter/FlexCenter";
 import FlexEnd from "components/commons/FlexEnd/FlexEnd";
+import DayCard from "components/edt/DayCard/DayCard";
+import WeekCard from "components/edt/WeekCard/WeekCard";
+import { makeStylesEdt } from "lunatic-edt";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { EdtRoutesNameEnum, getNavigatePath } from "routes/EdtRoutes";
@@ -11,43 +13,36 @@ import { EdtRoutesNameEnum, getNavigatePath } from "routes/EdtRoutes";
 const HomePage = () => {
     const { t } = useTranslation();
     const navigate = useNavigate();
+    const { classes } = useStyles();
     return (
         <>
             <FlexEnd>
                 <Button
+                    color="secondary"
                     startIcon={<HelpIcon />}
                     onClick={() => navigate(getNavigatePath(EdtRoutesNameEnum.HELP))}
                 >
                     {t("page.home.navigation.link-help-label")}
                 </Button>
-                {/* DEV : this will be removed */}
-                <Button
-                    startIcon={<CodeIcon />}
-                    onClick={() => navigate(getNavigatePath(EdtRoutesNameEnum.ORCHESTRATOR))}
-                >
-                    Orchestrator
-                </Button>
-                <Button
-                    startIcon={<CodeIcon />}
-                    onClick={() => navigate(getNavigatePath(EdtRoutesNameEnum.ACTIVITY))}
-                >
-                    Activity
-                </Button>
-                <Button
-                    startIcon={<CodeIcon />}
-                    onClick={() => navigate(getNavigatePath(EdtRoutesNameEnum.WHO_ARE_YOU))}
-                >
-                    Who are you
-                </Button>
             </FlexEnd>
-            <div>
-                <FlexCenter>
+            <Box>
+                <FlexCenter className={classes.spacing}>
                     <img src={reminder_note} alt={t("accessibility.asset.reminder-notes-alt")} />
                 </FlexCenter>
-                <header>{t("page.home.welcome")}</header>
-            </div>
+                <DayCard labelledBy={""} describedBy={""} onClick={() => console.log("dayCard")} />
+                <DayCard labelledBy={""} describedBy={""} onClick={() => console.log("dayCard")} />
+                <DayCard labelledBy={""} describedBy={""} onClick={() => console.log("dayCard")} />
+                <WeekCard labelledBy={""} describedBy={""} onClick={() => console.log("weekCard")} />
+                <WeekCard labelledBy={""} describedBy={""} onClick={() => console.log("weekCard")} />
+            </Box>
         </>
     );
 };
+
+const useStyles = makeStylesEdt({ "name": { HomePage } })(() => ({
+    spacing: {
+        margin: "2rem 0",
+    },
+}));
 
 export default HomePage;
