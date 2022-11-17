f696ca61ef72e2cc845e2a511482d8e4359ea5b5
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-11-17 09:40:00 +0100

    fix(sprint3): new res, clean and fix

---
+++ src/interface/lunatic/Lunatic.ts
@@ -0,0 +1,36 @@
+export interface Collected {
+    COLLECTED: string | boolean | null;
+    EDITED: any;
+    FORCED: any;
+    INPUTED: any;
+    PREVIOUS: any;
+}
+
+export interface LunaticData {
+    id?: string;
+    EXTERNAL?: any;
+    CALCULATED?: any;
+    COLLECTED?: { [key: string]: Collected };
+}
+
+export interface LunaticModel {
+    variables: LunaticModelVariable[];
+    components: LunaticModelComponent[];
+}
+
+export interface LunaticModelComponent {
+    bindingDependencies?: string[];
+    page: string;
+}
+
+export interface LunaticModelVariable {
+    values?: Collected;
+    variableType: string;
+    name: string;
+}
+
+export interface OrchestratorContext {
+    source: object;
+    data: LunaticData;
+    idSurvey: string;
+}
