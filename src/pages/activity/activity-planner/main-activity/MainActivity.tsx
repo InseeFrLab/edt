344bd0a593283c2bf6253aec0561c9cb3183d96f
Alexis Laulagnet <alexis.laulagnet@keyconsulting.fr>
2022-12-06 16:42:20 +0100

    feat(Activity): fix pictures and save

--- src/pages/activity/activity-planner/main-activity/MainActivity.tsx
+++ src/pages/activity/activity-planner/main-activity/MainActivity.tsx
@@ -9,14 +9,14 @@ import { getNextLoopPage, getPreviousLoopPage, getStepData } from "service/loop-
 import { getCurrentNavigatePath, getLoopParameterizedNavigatePath } from "service/navigation-service";
 import { saveData } from "service/survey-service";
 
-import catIcon1 from "assets/illustration/activity-categories/1.svg";
-import catIcon2 from "assets/illustration/activity-categories/2.svg";
-import catIcon3 from "assets/illustration/activity-categories/3.svg";
-import catIcon4 from "assets/illustration/activity-categories/4.svg";
-import catIcon5 from "assets/illustration/activity-categories/5.svg";
-import catIcon6 from "assets/illustration/activity-categories/6.svg";
-import catIcon7 from "assets/illustration/activity-categories/7.svg";
-import catIcon8 from "assets/illustration/activity-categories/8.svg";
+import catIcon100 from "assets/illustration/activity-categories/1.svg";
+import catIcon200 from "assets/illustration/activity-categories/2.svg";
+import catIcon300 from "assets/illustration/activity-categories/3.svg";
+import catIcon400 from "assets/illustration/activity-categories/4.svg";
+import catIcon440 from "assets/illustration/activity-categories/5.svg";
+import catIcon500 from "assets/illustration/activity-categories/6.svg";
+import catIcon650 from "assets/illustration/activity-categories/7.svg";
+import catIcon600 from "assets/illustration/activity-categories/8.svg";
 
 import iconNoResult from "assets/illustration/error/puzzle.svg";
 import activitesAutoCompleteRef from "activitesAutoCompleteRef.json";
@@ -39,16 +39,16 @@ const MainActivityPage = () => {
     const [displayStepper, setDisplayStepper] = React.useState<boolean>(true);
 
     const specificProps: ActivitySelecterSpecificProps = {
-        categoriesIcons: [
-            catIcon1,
-            catIcon2,
-            catIcon3,
-            catIcon4,
-            catIcon5,
-            catIcon6,
-            catIcon7,
-            catIcon8,
-        ],
+        categoriesIcons: {
+            "100": catIcon100,
+            "200": catIcon200,
+            "300": catIcon300,
+            "400": catIcon400,
+            "440": catIcon440,
+            "500": catIcon500,
+            "650": catIcon650,
+            "600": catIcon600,
+        },
         clickableListIconNoResult: iconNoResult,
         activitesAutoCompleteRef: activitesAutoCompleteRef,
         backClickEvent: backClickEvent,
