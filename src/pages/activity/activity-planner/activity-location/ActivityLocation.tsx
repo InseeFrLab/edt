f7733195167b1919ee385651a49e0f30746db3b6
Alexis Laulagnet <alexis.laulagnet@keyconsulting.fr>
2022-12-08 15:46:29 +0100

    feat(Location): selecter

--- src/pages/activity/activity-planner/activity-location/ActivityLocation.tsx
+++ src/pages/activity/activity-planner/activity-location/ActivityLocation.tsx
@@ -1,7 +1,9 @@
 import FlexCenter from "components/commons/FlexCenter/FlexCenter";
 import LoopSurveyPage from "components/commons/LoopSurveyPage/LoopSurveyPage";
 import { OrchestratorContext } from "interface/lunatic/Lunatic";
+import { LocationSelecterSpecificProps } from "lunatic-edt";
 import { callbackHolder, OrchestratorForStories } from "orchestrator/Orchestrator";
+import { useState } from "react";
 import { useNavigate, useOutletContext, useParams } from "react-router-dom";
 import { EdtRoutesNameEnum } from "routes/EdtRoutesMapping";
 import { getLoopInitialPage, LoopEnum } from "service/loop-service";
@@ -9,13 +11,60 @@ import { getNextLoopPage, getPreviousLoopPage, getStepData } from "service/loop-
 import { getCurrentNavigatePath, getLoopParameterizedNavigatePath } from "service/navigation-service";
 import { FieldNameEnum, getValue, saveData } from "service/survey-service";
 
+import option1 from "assets/illustration/locations/1.svg";
+import option2 from "assets/illustration/locations/2.svg";
+import option3 from "assets/illustration/locations/3.svg";
+import option4 from "assets/illustration/locations/4.svg";
+import option5 from "assets/illustration/locations/5.svg";
+import option6 from "assets/illustration/locations/6.svg";
+import { useTranslation } from "react-i18next";
+
 const ActivityLocationPage = () => {
     const navigate = useNavigate();
+    const { t } = useTranslation();
     const context = useOutletContext() as OrchestratorContext;
     const currentPage = EdtRoutesNameEnum.ACTIVITY_LOCATION;
     const stepData = getStepData(currentPage);
     const paramIteration = useParams().iteration;
     const currentIteration = paramIteration ? +paramIteration : 0;
+
+    const [backClickEvent, setBackClickEvent] = useState<React.MouseEvent>();
+    const [nextClickEvent, setNextClickEvent] = useState<React.MouseEvent>();
+
+    const specificProps: LocationSelecterSpecificProps = {
+        optionsIcons: {
+            "1": option1,
+            "2": option2,
+            "3": option3,
+            "4": option4,
+            "5": option5,
+            "6": option6,
+        },
+        backClickEvent: backClickEvent,
+        nextClickEvent: nextClickEvent,
+        backClickCallback: () => {
+            saveData(context.idSurvey, callbackHolder.getData()).then(() => {
+                const hasSecondaryActivity = getValue(
+                    context.idSurvey,
+                    FieldNameEnum.WITHSECONDARYACTIVITY,
+                    currentIteration,
+                );
+                if (hasSecondaryActivity) {
+                    loopNavigate(EdtRoutesNameEnum.SECONDARY_ACTIVITY_SELECTION);
+                } else {
+                    loopNavigate(getPreviousLoopPage(currentPage));
+                }
+            });
+        },
+        nextClickCallback: () => {
+            saveAndLoopNavigate(getNextLoopPage(currentPage));
+        },
+        labels: {
+            alertMessage: t("component.location-selecter.alert-message"),
+            alertIgnore: t("component.location-selecter.alert-ignore"),
+            alertComplete: t("component.location-selecter.alert-complete"),
+        },
+    };
 
     const loopNavigate = (page: EdtRoutesNameEnum) => {
         navigate(
@@ -40,23 +89,12 @@ const ActivityLocationPage = () => {
         });
     };
 
-    const onNext = () => {
-        saveAndLoopNavigate(getNextLoopPage(currentPage));
+    const onNext = (e: React.MouseEvent) => {
+        setNextClickEvent(e);
     };
 
-    const onPrevious = () => {
-        saveData(context.idSurvey, callbackHolder.getData()).then(() => {
-            const hasSecondaryActivity = getValue(
-                context.idSurvey,
-                FieldNameEnum.WITHSECONDARYACTIVITY,
-                currentIteration,
-            );
-            if (hasSecondaryActivity) {
-                loopNavigate(EdtRoutesNameEnum.SECONDARY_ACTIVITY_SELECTION);
-            } else {
-                loopNavigate(getPreviousLoopPage(currentPage));
-            }
-        });
+    const onPrevious = (e: React.MouseEvent) => {
+        setBackClickEvent(e);
     };
 
     return (
@@ -77,6 +115,7 @@ const ActivityLocationPage = () => {
                     page={getLoopInitialPage(LoopEnum.ACTIVITY)}
                     subPage={(stepData.stepNumber + 1).toString()}
                     iteration={currentIteration}
+                    componentSpecificProps={specificProps}
                 ></OrchestratorForStories>
             </FlexCenter>
         </LoopSurveyPage>
