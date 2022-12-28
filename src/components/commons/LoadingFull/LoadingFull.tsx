91bdbc086e2c34cbdccdba415b1619451a02fb6f
laila abjil <laila.abjil@keyconsulting.fr>
2022-12-28 09:21:12 +0100

    feat(91): fix loading page

--- src/components/commons/LoadingFull/LoadingFull.tsx
+++ src/components/commons/LoadingFull/LoadingFull.tsx
@@ -1,5 +1,10 @@
 import { CircularProgress } from "@mui/material";
+import reminder_note from "assets/illustration/reminder-note.svg";
 import FlexCenter from "components/commons/FlexCenter/FlexCenter";
+import PageIcon from "components/commons/PageIcon/PageIcon";
+import { makeStylesEdt } from "lunatic-edt";
+import { useTranslation } from "react-i18next";
+
 interface LoadingFullProps {
     message: string;
     thanking?: string;
@@ -7,10 +12,19 @@ interface LoadingFullProps {
 
 const LoadingFull = (props: LoadingFullProps) => {
     const { message, thanking } = props;
+    const { t } = useTranslation();
+    const { classes } = useStyles();
+
     return (
         <>
             <FlexCenter>
-                <CircularProgress />
+                <PageIcon
+                    srcIcon={reminder_note}
+                    altIcon={t("accessibility.asset.reminder-notes-alt")}
+                />
+            </FlexCenter>
+            <FlexCenter>
+                <CircularProgress thickness={2} className={classes.loading} />
             </FlexCenter>
             <FlexCenter>
                 <h2>{message}</h2>
@@ -19,5 +33,12 @@ const LoadingFull = (props: LoadingFullProps) => {
         </>
     );
 };
+
+const useStyles = makeStylesEdt({ "name": { LoadingFull } })({
+    loading: {
+        width: "117px !important",
+        height: "117px !important",
+    },
+});
 
 export default LoadingFull;
