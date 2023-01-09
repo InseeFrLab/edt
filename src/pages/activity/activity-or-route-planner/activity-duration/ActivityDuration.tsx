f2ec620dde0c65e1013e78c1f533529b7a3e34bd
laila abjil <laila.abjil@keyconsulting.fr>
2023-01-09 20:07:14 +0100

    fix(duration): add alert when end time after start time

--- src/pages/activity/activity-or-route-planner/activity-duration/ActivityDuration.tsx
+++ src/pages/activity/activity-or-route-planner/activity-duration/ActivityDuration.tsx
@@ -1,9 +1,9 @@
 import FlexCenter from "components/commons/FlexCenter/FlexCenter";
 import LoopSurveyPage from "components/commons/LoopSurveyPage/LoopSurveyPage";
 import { OrchestratorContext } from "interface/lunatic/Lunatic";
-import { Alert, TimepickerSpecificProps } from "lunatic-edt";
+import { Alert, makeStylesEdt, TimepickerSpecificProps } from "lunatic-edt";
 import { callbackHolder, OrchestratorForStories } from "orchestrator/Orchestrator";
-import { useState } from "react";
+import { Fragment, useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate, useOutletContext, useParams } from "react-router-dom";
 import { EdtRoutesNameEnum } from "routes/EdtRoutesMapping";
@@ -16,13 +16,17 @@ import {
 } from "service/navigation-service";
 import { getActivitiesOrRoutes } from "service/survey-activity-service";
 import { FieldNameEnum, getValue, saveData } from "service/survey-service";
-
 import errorIcon from "assets/illustration/error/activity.svg";
+import dayjs, { Dayjs } from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import { IconButton, Snackbar } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 const ActivityDurationPage = () => {
     const navigate = useNavigate();
     const { t } = useTranslation();
     const context: OrchestratorContext = useOutletContext();
+    const { classes } = useStyles();
     setEnviro(context, useNavigate(), callbackHolder);
 
     const currentPage = EdtRoutesNameEnum.ACTIVITY_DURATION;
@@ -33,6 +37,10 @@ const ActivityDurationPage = () => {
     const isRoute = getValue(context.idSurvey, FieldNameEnum.ISROUTE, currentIteration) as boolean;
 
     const [isAlertDisplayed, setIsAlertDisplayed] = useState<boolean>(false);
+    const [snackbarText, setSnackbarText] = useState<string | undefined>(undefined);
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+    const [lastEndTime, setLastEndTime] = useState(dayjs());
+
     const alertLabels = {
         boldContent: t("page.alert-when-quit.activity.alert-content-bold"),
         content: !isRoute
@@ -47,32 +55,95 @@ const ActivityDurationPage = () => {
         defaultValue: true,
     };
 
+    let startTimeDay: Dayjs;
+    let endTimeDay: Dayjs;
+
+    const isAfterEndTime = () => {
+        const data = callbackHolder.getData();
+        let isAfter = false;
+        if (data) {
+            const startTime = data.COLLECTED?.[FieldNameEnum.STARTTIME]?.COLLECTED as string[];
+            const endTime = data.COLLECTED?.[FieldNameEnum.ENDTIME]?.COLLECTED as string[];
+
+            dayjs.extend(customParseFormat);
+            if (startTime && endTime) {
+                startTimeDay = dayjs(startTime[currentIteration], "HH:mm");
+                endTimeDay = dayjs(endTime[currentIteration], "HH:mm");
+                if (startTimeDay.isAfter(endTimeDay)) {
+                    isAfter = true;
+                }
+            }
+        }
+        return isAfter;
+    };
+
+    const endTimeAfterStartTime = (isAfter: boolean) => {
+        let skip = false;
+        if (isAfter) {
+            setSnackbarText(t("page.activity-duration.hour-alert"));
+            if (endTimeDay.isSame(lastEndTime)) {
+                skip = true;
+            }
+            setLastEndTime(endTimeDay);
+        } else {
+            skip = true;
+        }
+        setOpenSnackbar(!skip);
+        return skip;
+    };
+
     const onNext = () => {
-        saveData(context.idSurvey, callbackHolder.getData()).then(() => {
-            navigate(
-                getLoopParameterizedNavigatePath(
-                    getNextLoopPage(currentPage, context.isRoute),
-                    LoopEnum.ACTIVITY_OR_ROUTE,
-                    currentIteration,
-                ),
-            );
-        });
+        const isAfter = isAfterEndTime();
+        const skip = endTimeAfterStartTime(isAfter);
+
+        if (isAfter) {
+            setLastEndTime(endTimeDay);
+        }
+
+        if ((skip && isAfter) || !isAfter) {
+            saveData(context.idSurvey, callbackHolder.getData()).then(() => {
+                navigate(
+                    getLoopParameterizedNavigatePath(
+                        getNextLoopPage(currentPage, context.isRoute),
+                        LoopEnum.ACTIVITY_OR_ROUTE,
+                        currentIteration,
+                    ),
+                );
+            });
+        }
     };
 
     const onClose = (forceQuit: boolean) => {
         const isCompleted = getValue(context.idSurvey, FieldNameEnum.ISCOMPLETED, currentIteration);
-        if (!isCompleted) {
-            if (forceQuit) {
-                saveData(context.idSurvey, callbackHolder.getData()).then(() => {
-                    navToActivitRouteHome();
-                });
+        if (!openSnackbar) {
+            if (!isCompleted) {
+                if (forceQuit) {
+                    saveData(context.idSurvey, callbackHolder.getData()).then(() => {
+                        navToActivitRouteHome();
+                    });
+                } else {
+                    setIsAlertDisplayed(true);
+                }
             } else {
-                setIsAlertDisplayed(true);
+                navToActivitRouteHome();
             }
-        } else {
-            navToActivitRouteHome();
         }
     };
+
+    const handleCloseSnackBar = useCallback((event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        setOpenSnackbar(false);
+    }, []);
+
+    const snackbarAction = (
+        <Fragment>
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleCloseSnackBar}>
+                <CloseIcon fontSize="small" />
+            </IconButton>
+        </Fragment>
+    );
 
     return (
         <LoopSurveyPage
@@ -92,6 +163,17 @@ const ActivityDurationPage = () => {
                     icon={errorIcon}
                     errorIconAlt={t("page.activity-duration.alt-alert-icon")}
                 ></Alert>
+                <Snackbar
+                    className={classes.snackbar}
+                    open={openSnackbar}
+                    onClose={handleCloseSnackBar}
+                    message={snackbarText}
+                    action={snackbarAction}
+                    anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "center",
+                    }}
+                />
                 <OrchestratorForStories
                     source={context.source}
                     data={context.data}
@@ -105,5 +187,15 @@ const ActivityDurationPage = () => {
         </LoopSurveyPage>
     );
 };
+
+const useStyles = makeStylesEdt({ "name": { ActivityDurationPage } })(theme => ({
+    snackbar: {
+        height: "30%",
+        "& .MuiSnackbarContent-root": {
+            backgroundColor: theme.palette.error.light,
+            color: theme.variables.alertActivity,
+        },
+    },
+}));
 
 export default ActivityDurationPage;
