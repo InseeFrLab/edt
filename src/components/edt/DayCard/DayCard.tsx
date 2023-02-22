58ab2f883b8a909f47693cc41c7531c83c31295e
laila abjil <laila.abjil@keyconsulting.fr>
2023-02-22 19:17:44 +0100

    feat(home): change css when isClosed the form

--- src/components/edt/DayCard/DayCard.tsx
+++ src/components/edt/DayCard/DayCard.tsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box";
 import PersonSunIcon from "assets/illustration/card/person-sun.svg";
+import PersonSunCloseIcon from "assets/illustration/card/person-sun-close.svg";
 import FlexCenter from "components/commons/FlexCenter/FlexCenter";
 import PourcentProgress from "components/edt/PourcentProgress/PourcentProgress";
 import { makeStylesEdt } from "lunatic-edt";
@@ -13,11 +14,12 @@ interface DayCardProps {
     firstName: string;
     surveyDate: string;
     idSurvey: string;
+    isClose: boolean;
 }
 
 const DayCard = (props: DayCardProps) => {
-    const { labelledBy, describedBy, onClick, firstName, surveyDate, idSurvey } = props;
-    const { classes } = useStyles();
+    const { labelledBy, describedBy, onClick, firstName, surveyDate, idSurvey, isClose } = props;
+    const { classes, cx } = useStyles();
     const { t } = useTranslation();
     const progressActivity = getScore(idSurvey, t);
 
@@ -26,20 +28,28 @@ const DayCard = (props: DayCardProps) => {
             <Box
                 aria-labelledby={labelledBy}
                 aria-describedby={describedBy}
-                className={classes.dayCardBox}
+                className={cx(classes.dayCardBox, isClose ? classes.closeCardBox : "")}
                 onClick={onClick}
             >
                 <Box className={classes.leftBox}>
-                    <Box className={classes.iconBox}>
-                        <img src={PersonSunIcon} alt={t("accessibility.asset.card.person-sun-alt")} />
+                    <Box className={cx(classes.iconBox, isClose ? classes.closeIconBox : "")}>
+                        <img
+                            src={isClose ? PersonSunCloseIcon : PersonSunIcon}
+                            alt={t("accessibility.asset.card.person-sun-alt")}
+                        />
                     </Box>
                     <Box>
                         <Box>{surveyDate}</Box>
                         <Box>{firstName}</Box>
                     </Box>
                 </Box>
-                <Box className={classes.scoreBox}>
-                    <PourcentProgress labelledBy={""} describedBy={""} progress={progressActivity} />
+                <Box className={cx(classes.scoreBox)}>
+                    <PourcentProgress
+                        labelledBy={""}
+                        describedBy={""}
+                        progress={progressActivity}
+                        isClose={isClose}
+                    />
                 </Box>
             </Box>
         </FlexCenter>
@@ -61,12 +71,21 @@ const useStyles = makeStylesEdt({ "name": { DayCard } })(theme => ({
         marginTop: "1rem",
         cursor: "pointer",
     },
+    closeCardBox: {
+        color: theme.palette.secondary.main,
+        backgroundColor: theme.palette.secondary.light,
+        border: "1px solid ",
+        borderColor: theme.palette.secondary.dark,
+    },
     leftBox: {
         display: "flex",
         alignItems: "center",
     },
     iconBox: {
         marginRight: "1rem",
+    },
+    closeIconBox: {
+        color: theme.palette.secondary.main,
     },
     scoreBox: {
         color: theme.palette.secondary.main,
