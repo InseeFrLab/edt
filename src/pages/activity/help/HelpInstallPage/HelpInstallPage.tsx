fb59cdc6889e7057bec28052216c6dc3f8342922
Laila Abjil <laila.abjil@keyconsulting.fr>
2023-04-11 11:56:26 +0000

    fix(help): add version in install page

--- src/pages/activity/help/HelpInstallPage/HelpInstallPage.tsx
+++ src/pages/activity/help/HelpInstallPage/HelpInstallPage.tsx
@@ -1,5 +1,5 @@
 import { makeStylesEdt } from "@inseefrlab/lunatic-edt";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import install from "assets/illustration/install.svg";
 import FlexCenter from "components/commons/FlexCenter/FlexCenter";
 import { EdtRoutesNameEnum } from "enumerations/EdtRoutesNameEnum";
@@ -7,6 +7,7 @@ import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { getNavigatePath } from "service/navigation-service";
+import packageJson from "../../../../../package.json";
 
 const HelpInstallPage = () => {
     const { classes } = useStyles();
@@ -18,33 +19,53 @@ const HelpInstallPage = () => {
     }, []);
 
     return (
-        <Box className={classes.installBox}>
-            <FlexCenter className={classes.illustrationBox}>
-                <img src={install} alt={t("accessibility.asset.install-alt")} />
-            </FlexCenter>
-            <Box className={classes.textBox}>
-                <h2>{t("page.install.title")}</h2>
-                <p>{t("page.install.subtitle")}</p>
-                <p>{t("page.install.info")}</p>
-            </Box>
-            <Box className={classes.actionsBox}>
-                <Box className={classes.actionBox}>
-                    <Button className={classes.button} variant="contained" onClick={navToHelp}>
-                        {t("common.navigation.next")}
-                    </Button>
+        <Box> 
+            <Box className={classes.installBox}>
+                <FlexCenter className={classes.illustrationBox}>
+                    <img src={install} alt={t("accessibility.asset.install-alt")} />
+                </FlexCenter>
+                <Box className={classes.textBox}>
+                    <h2>{t("page.install.title")}</h2>
+                    <p>{t("page.install.subtitle")}</p>
+                    <p>{t("page.install.info")}</p>
+                </Box>
+                <Box className={classes.actionsBox}>
+                    <Box className={classes.actionBox}>
+                        <Button className={classes.button} variant="contained" onClick={navToHelp}>
+                            {t("common.navigation.next")}
+                        </Button>
+                    </Box>
                 </Box>
             </Box>
+            <br />
+            <Paper className={classes.footerBox} component="footer" square variant="outlined">
+                <Box>
+                    <Typography variant="caption" color="initial">
+                        <b>{t("common.version")}:</b> {packageJson.version} - {packageJson.dateVersion}
+                    </Typography>
+                </Box>
+            </Paper>
         </Box>
+        
     );
 };
 
 const useStyles = makeStylesEdt({ "name": { HelpInstallPage } })(theme => ({
-    installBox: { padding: "1rem" },
+    installBox: { 
+        padding: "1rem",
+        height: "90vh"
+    },
     illustrationBox: {},
     textBox: { textAlign: "center" },
     actionsBox: { display: "flex", flexDirection: "column", alignItems: "center" },
     actionBox: { maxWidth: "300px", margin: "0.5rem 0", width: "90%" },
     button: { width: "100%", backgroundColor: theme.palette.text.primary },
+    footerBox: {
+        height: "7vh",
+        display: "flex",
+        alignItems: "center",
+        padding: "1rem",
+    },
 }));
 
 export default HelpInstallPage;
