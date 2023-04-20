ebcd3506435ab2b56c017d848d1310408c2833b2
laila abjil <laila.abjil@keyconsulting.fr>
2023-04-20 12:55:51 +0200

    feat(accessibility): add alts

--- src/pages/activity/activity-or-route-planner/activity/activity-location/ActivityLocation.tsx
+++ src/pages/activity/activity-or-route-planner/activity/activity-location/ActivityLocation.tsx
@@ -8,17 +8,19 @@ import option6 from "assets/illustration/locations/6.svg";
 import LoopSurveyPageStep from "components/commons/LoopSurveyPage/LoopSurveyPageStep/LoopSurveyPageStep";
 import { EdtRoutesNameEnum } from "enumerations/EdtRoutesNameEnum";
 import { FieldNameEnum } from "enumerations/FieldNameEnum";
+import { useTranslation } from "react-i18next";
 import { getPlaceRef } from "service/referentiel-service";
 
 const ActivityLocationPage = () => {
+    const { t } = useTranslation();
     const specifiquesProps = {
         optionsIcons: {
-            "11": option1,
-            "12": option2,
-            "14": option3,
-            "15": option4,
-            "13": option5,
-            "16": option6,
+            "11": { icon: option1, altIcon: t("accessibility.assets.place.categories.option1-alt") },
+            "12": { icon: option2, altIcon: t("accessibility.assets.place.categories.option1-alt") },
+            "14": { icon: option3, altIcon: t("accessibility.assets.place.categories.option1-alt") },
+            "15": { icon: option4, altIcon: t("accessibility.assets.place.categories.option1-alt") },
+            "13": { icon: option5, altIcon: t("accessibility.assets.place.categories.option1-alt") },
+            "16": { icon: option6, altIcon: t("accessibility.assets.place.categories.option1-alt") },
         },
         referentiel: getPlaceRef(),
     };
--- src/pages/activity/help/HelpCheckbox/HelpCheckbox.tsx
+++ src/pages/activity/help/HelpCheckbox/HelpCheckbox.tsx
@@ -13,7 +13,7 @@ import LoopSurveyPage from "components/commons/LoopSurveyPage/LoopSurveyPage";
 import { EdtRoutesNameEnum } from "enumerations/EdtRoutesNameEnum";
 import { LoopEnum } from "enumerations/LoopEnum";
 import { SourcesEnum } from "enumerations/SourcesEnum";
-import { callbackHolder, OrchestratorForStories } from "orchestrator/Orchestrator";
+import { OrchestratorForStories, callbackHolder } from "orchestrator/Orchestrator";
 import React, { useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
@@ -39,8 +39,6 @@ const HelpCheckbox = () => {
     const currentPage = EdtRoutesNameEnum.WITH_SOMEONE_SELECTION;
     const stepData = getStepData(currentPage);
     const source = getSource(SourcesEnum.ACTIVITY_SURVEY);
-    /*const idSurvey = getIdSurveyContext(SurveysIdsEnum.ACTIVITY_SURVEYS_IDS);
-    const data = getData(idSurvey || "");*/
     const data = mockData();
 
     const [helpStep, setHelpStep] = React.useState(1);
@@ -150,11 +148,26 @@ const HelpCheckbox = () => {
             console.log("");
         },
         optionsIcons: {
-            "1": coupleIcon,
-            "2": parentsIcon,
-            "3": childIcon,
-            "4": otherKnownIcon,
-            "5": otherIcon,
+            "1": {
+                icon: coupleIcon,
+                altIcon: t("accessibility.assets.with-someone.categories.couple-alt"),
+            },
+            "2": {
+                icon: parentsIcon,
+                altIcon: t("accessibility.assets.with-someone.categories.parents-alt"),
+            },
+            "3": {
+                icon: childIcon,
+                altIcon: t("accessibility.assets.with-someone.categories.child-alt"),
+            },
+            "4": {
+                icon: otherKnownIcon,
+                altIcon: t("accessibility.assets.with-someone.categories.other-know-alt"),
+            },
+            "5": {
+                icon: otherIcon,
+                altIcon: t("accessibility.assets.with-someone.categories.other-alt"),
+            },
         },
         displayStepper: false,
         helpStep: helpStep,
