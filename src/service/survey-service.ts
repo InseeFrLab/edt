053d5598d17d8237199a57a045b292de532b545f
laila abjil <laila.abjil@keyconsulting.fr>
2023-05-04 15:52:14 +0200

    fix(header): fix responsive header

--- src/service/survey-service.ts
+++ src/service/survey-service.ts
@@ -503,7 +503,7 @@ const getPrintedSurveyDate = (idSurvey: string, surveyParentPage?: EdtRoutesName
         const capitalizedDayName = dayName.charAt(0).toUpperCase() + dayName.slice(1);
 
         const splittedDate = savedSurveyDate.split("-");
-        return label + " - " + capitalizedDayName + " " + [splittedDate[2], splittedDate[1]].join("/");
+        return capitalizedDayName + " " + [splittedDate[2], splittedDate[1]].join("/");
     } else {
         return label + " 1";
     }
