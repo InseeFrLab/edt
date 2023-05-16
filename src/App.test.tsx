7cdc8b8c86c0dabb5a531e6675e0e79b1d2952fa
laila abjil <laila.abjil@keyconsulting.fr>
2023-05-16 17:52:27 +0200

    feat(test): config jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,32 @@
-import "fake-indexeddb/auto";
-import App from "App";
 import { render } from "@testing-library/react";
+import App from "App.tsx";
+
+jest.mock("oidc-react", () => ({
+    useAuth: () => {
+        return {
+            isLoading: false,
+            user: { sub: "foobar", email: "foo@bar" },
+            isAuthenticated: true,
+            loginWithRedirect: jest.fn(),
+            logout: jest.fn(),
+        };
+    },
+}));
+
+jest.mock("react-i18next", () => ({
+    // this mock makes sure any components using the translate hook can use it without a warning being shown
+    useTranslation: () => {
+        return {
+            t: (str: any) => str,
+            i18n: {
+                changeLanguage: () =>
+                    new Promise(() => {
+                        console.log("");
+                    }),
+            },
+        };
+    },
+}));
 
 test("FAKE", () => {
     render(<App />);
