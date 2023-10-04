21ab80b04f85b0df84d337cc2a5a023f5674671f
Laila <laila.abjil@keyconsulting.fr>
2023-10-04 11:53:29 +0200

    fix(data): fix select data of lunatic or database

--- src/orchestrator/Orchestrator.tsx
+++ src/orchestrator/Orchestrator.tsx
@@ -87,7 +87,7 @@ export const OrchestratorForStories = (props: OrchestratorProps) => {
                         if (collected && editedSaved && Array.isArray(collected)) {
                             let maxLenght = Number(localStorage.getItem("loopSize")) ?? 0;
                             for (let i = 0; i < maxLenght; i++) {
-                                if (collected[i] == null) {
+                                if (i != iteration) {
                                     collected[i] = editedSaved[i];
                                 }
                             }
