fc4b9d1f2b69e80ccdf7f092cfd6733f810259f9
Laila Abjil <laila.abjil@keyconsulting.fr>
2024-01-29 19:01:11 +0100

    fix(responsive): fix logout

--- src/pages/home-reviewer/HomeReviewer.tsx
+++ src/pages/home-reviewer/HomeReviewer.tsx
@@ -91,6 +91,7 @@ const HomeReviewerPage = () => {
                     </>
                 </Box>
                 <Button
+                    className={classes.logoutBox}
                     color="secondary"
                     startIcon={
                         <img
@@ -157,6 +158,9 @@ const useStyles = makeStylesEdt({ "name": { HomeReviewerPage } })(theme => ({
     logoBox: {
         paddingLeft: "1rem",
         paddingTop: "0.5rem",
+    },
+    logoutBox: {
+        height: "4rem",
     },
     logoImg: {
         width: "40px",
