4e5c4c921ea1db6f7ae6f86a6bde19b038c72573
Laila Abjil <laila.abjil@keyconsulting.fr>
2024-01-30 18:02:15 +0100

    fix(version): fix orch

--- src/orchestrator/Orchestrator.tsx
+++ src/orchestrator/Orchestrator.tsx
@@ -207,7 +207,7 @@ const getDataInterviewer = (getData: any, data: LunaticData | undefined, source?
             const dataOfField = dataCollected[prop];
             //set values edited with values in bdd, because we don't recover the edited part with lunatic
             if (dataOfField) {
-                dataOfField.COLLECTED = dataOfField.COLLECTED ?? data?.COLLECTED?.[prop].EDITED;
+                dataOfField.COLLECTED = dataOfField.COLLECTED ?? data?.COLLECTED?.[prop]?.EDITED;
             }
         });
     }
