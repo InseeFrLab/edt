f40a730f7091a0816e4ad54994f62769ec964d10
Laila Abjil <laila.abjil@keyconsulting.fr>
2024-02-28 18:05:45 +0100

    fix(offline): test offline images

--- src/components/edt/HelpMenu/HelpMenuInner.tsx
+++ src/components/edt/HelpMenu/HelpMenuInner.tsx
@@ -2,10 +2,9 @@ import { makeStylesEdt } from "@inseefrlab/lunatic-edt";
 import { Box, Button, Divider } from "@mui/material";
 import arrowForwardIos from "assets/illustration/mui-icon/arrow-forward-ios.svg";
 import close from "assets/illustration/mui-icon/close.svg";
-import download from "assets/illustration/mui-icon/download.svg";
+import download, { ReactComponent as DownloadObject } from "assets/illustration/mui-icon/download.svg";
 import expandLess from "assets/illustration/mui-icon/expand-less.svg";
-import mail from "assets/illustration/mui-icon/mail.svg";
-import rebaseEdit from "assets/illustration/mui-icon/rebase_edit.svg";
+import mail, { ReactComponent as MailObject } from "assets/illustration/mui-icon/mail.svg";
 import FlexCenter from "components/commons/FlexCenter/FlexCenter";
 import { useTranslation } from "react-i18next";
 import { getDeviceNavigatorIsAvaiableForInstall } from "utils/utils";
@@ -48,6 +47,7 @@ const HelpMenuInner = (props: HelpMenuInnerProps) => {
         <Box className={cx(className, getClassDevice(classes.modalMobile, classes.modalDefault))}>
             <Box id="contact" className={getClassNavigateBox()} onClick={onClickContact}>
                 <Box className={getClassIconBox()}>
+                    <MailObject></MailObject>
                     <img src={mail} alt={t("accessibility.asset.help-menu.contact-alt")} />
                 </Box>
                 <Box className={getClassTextBox()}>
@@ -65,6 +65,7 @@ const HelpMenuInner = (props: HelpMenuInnerProps) => {
                 <>
                     <Box id="install" className={getClassNavigateBox()} onClick={onClickInstall}>
                         <Box className={getClassIconBox()}>
+                            <DownloadObject></DownloadObject>
                             <img src={download} alt={t("accessibility.asset.help-menu.install-alt")} />
                         </Box>
                         <Box className={getClassTextBox()}>
@@ -82,7 +83,11 @@ const HelpMenuInner = (props: HelpMenuInnerProps) => {
             )}
             <Box id="help" className={getClassNavigateBox()} onClick={onClickHelp}>
                 <Box className={getClassIconBox()}>
-                    <img src={rebaseEdit} alt={t("accessibility.asset.help-menu.help-alt")} />
+                    <img
+                        src="/assets/illustration/mui-icon/rebase_edit.svg"
+                        alt={t("accessibility.asset.help-menu.help-alt")}
+                    />
+                    <img src="/icons/contact.svg" alt={t("accessibility.asset.help-menu.help-alt")} />
                 </Box>
                 <Box className={getClassTextBox()}>
                     <p>{t("component.help-menu.help-label")}</p>
