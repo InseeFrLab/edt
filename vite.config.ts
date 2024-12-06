3acf1785506f72156c8ddb1f21cee7030e4b850b
Jonathan <contact@grafikart.fr>
2024-12-06 18:24:50 +0100

    fix: vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { VitePWA } from "vite-plugin-pwa";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // @ts-expect-error Test is only for vitest
     test: {
         environment: "jsdom",
     },
